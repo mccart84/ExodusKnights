c43ca054290fe6176efb7454f2860677d44c41e4
Ryan McCarty <ryan.mccarty90@hotmail.com>
2018-10-21 19:53:13 -0400

    fixing missing dependancy

--- ExodusKnights/ClientApp/src/components/Home.js
+++ ExodusKnights/ClientApp/src/components/Home.js
@@ -5,8 +5,8 @@ const Home = props => (
     <div>
         <div style={{ margin: 'auto', width: '100%', textAlign: 'center', color: 'white' }}>
             <h1>Welcome to Exodus Knights Free Company on Coeurl</h1>
-            <h4>Looking for a fresh start or a new beginning? Come join Exodus Knights! We are a chill, casual-midcore fc with active raiders, crafters and helpful members!</h4>
-            <h4>Events, raffles, and weekly map runs!</h4>
+            <h4>Looking for a fresh start or a new beginning? Come join Exodus Knights! We are a chill, casual-midcore free company with active raiders, crafters and helpful members!</h4>
+            <h4>We hold events regularly!</h4>
         </div>
         <div class="row" style={{marginTop: '10%'}}>
             <div class="column">
